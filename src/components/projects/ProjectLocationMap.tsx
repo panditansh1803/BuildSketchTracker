364f848f6a3bd15f65c709f603bf0c598f8a0f90
panditansh1803 <vashisthaansh1803@gmail.com>
2026-02-06 17:44:32 +0530

    fix(map): Add missing Leaflet CSS import

--- src/components/projects/ProjectLocationMap.tsx
+++ src/components/projects/ProjectLocationMap.tsx
@@ -2,6 +2,7 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
 
